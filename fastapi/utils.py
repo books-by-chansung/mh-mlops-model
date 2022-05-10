4d9da176930d41b05833479f7acf2e35348802e4
deep-diver <deep.diver.csp@gmail.com>
2022-05-10 09:37:41 +0900

    update comment

--- fastapi/utils.py
+++ fastapi/utils.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 GH_TOEKN_KEY = "g_token"
 
 """
-    $ gpg -o .env.gpg -c .env --passphrase-fd 0
+    $ gpg -o .env.gpg -c .env
     $ echo "passphrase" | gpg --passphrase-fd 0 -o .env -d .env.gpg
 """
 load_dotenv('.env')
